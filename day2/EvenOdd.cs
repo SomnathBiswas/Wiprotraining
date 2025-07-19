2eaf9a282e893128943a4975d27464c99edc714e
Somnath Biswas <somnathbiswas576@gmail.com>
2025-07-19 15:59:25 +0530

    day 2 added

---
+++ day2/EvenOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program
+{
+    public class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter the number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine(number + " " + "is an Even Number");
+            }
+
+            else
+            {
+                Console.WriteLine(number + " " + "is odd number");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
