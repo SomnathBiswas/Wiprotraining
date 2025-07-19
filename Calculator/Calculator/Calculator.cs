682f54cdaec4e971337a8cb6ead273ed0c41ee3e
Somnath Biswas <somnathbiswas576@gmail.com>
2025-07-19 15:58:20 +0530

    day 2 added

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day1prog
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+
+            Calculations C = new Calculations();
+            Console.WriteLine("Welcome To Calculator APP");
+            C.Display();
+            
+
+
+        }
+    }
+}
+
+
+
+
+
+// using System;
+// public class Calculator
+// {
+//    public static void Main(string[] args)
+//     {
+//         Program2 p = new Program2();
+//         Console.WriteLine("Welcome To Calculator APP");
+//         p.Display();
+//         Console.ReadKey();
+//     }
+// }
