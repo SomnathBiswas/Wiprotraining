547fc2bab189276fc2a90f0035dc8904c0af9a8b
Somnath Biswas <somnathbiswas576@gmail.com>
2025-07-21 14:10:59 +0530

    Arrays are added

---
+++ day3array1/Program.cs
@@ -0,0 +1,22 @@
+﻿//create a single dimensional array to store 5 student names by taking input from an user
+//and then display
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] names = new string[5];
+        Console.WriteLine("Enter 5 student names:");
+        for (int i = 0; i < 5; i++)
+        {
+            names[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Student names are:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(" "+names[i]);
+        }
+    }
+}
