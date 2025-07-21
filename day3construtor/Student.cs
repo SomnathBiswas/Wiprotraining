10f6b5f10615c53da438bb351dfd4a0773a6ab01
Somnath Biswas <somnathbiswas576@gmail.com>
2025-07-21 17:03:26 +0530

    Constuctor added

---
+++ day3construtor/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StudentProgram;  // Package
+class Student
+{
+
+    // Properties GET SET
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int[] SubjectMarks { get; set; }
+
+
+}
